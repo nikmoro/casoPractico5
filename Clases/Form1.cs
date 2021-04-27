72f4367866eb36cce988c8b544986d2eab67b10e
Osiel Nicolás <chilparosales@gmail.com>
2021-04-26 23:31:05 -0500

    Modificaciones y documentación del código

--- Clases/Form1.cs
+++ Clases/Form1.cs
@@ -12,42 +12,46 @@ namespace Clases
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Método que consume la API del clima
+        /// </summary>
         private void btnPrueba_Click(object sender, EventArgs e)
         {
-            dynamic respuesta = dBApi.Get("https://goweather.herokuapp.com/weather/"+txtEmail.Text+"");
+            dynamic respuesta = dBApi.Get("https://goweather.herokuapp.com/weather/" + txtEmail.Text + "");
             txtNombreGET.Text = respuesta.forecast[0].temperature.ToString();
             txtApellidoGET.Text = respuesta.forecast[0].wind.ToString();
         }
 
+        /// <summary>
+        /// Método que consume la API de Pokémon
+        /// </summary>
         private void button1_Click(object sender, EventArgs e)
         {
             dynamic respuesta = dBApi.Get("https://pokeapi.co/api/v2/pokemon/" + txtpokemon.Text + "");
             pictureBox2.ImageLocation = respuesta.sprites.front_default.ToString();
             lblpokemon.Text = respuesta.forms[0].name.ToString();
             lblpoke.Text = respuesta.id.ToString();
-            
         }
 
+        /// <summary>
+        /// Botón que anima el pokémon
+        /// </summary>
+        private void btnanimar_Click(object sender, EventArgs e)
+        {
+            dynamic respuesta = dBApi.Get("https://pokeapi.co/api/v2/pokemon/" + txtpokemon.Text + "");
+            pictureBox2.ImageLocation = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/" + lblpoke.Text + ".gif";
+        }
+
+        /// <summary>
+        /// Método que consume la API de Rick y Morty
+        /// </summary>
         private void btnJoke_Click(object sender, EventArgs e)
         {
-            dynamic respuesta = dBApi.Get("https://rickandmortyapi.com/api/character/"+txtpoke.Text+"");
+            dynamic respuesta = dBApi.Get("https://rickandmortyapi.com/api/character/" + txtpoke.Text + "");
             pictureBox1.ImageLocation = respuesta.image.ToString();
             textBoxNombre.Text = respuesta.name.ToString();
             textBoxEstado.Text = respuesta.status.ToString();
             textBoxGenero.Text = respuesta.gender.ToString();
         }
-
-        private void btnanimar_Click(object sender, EventArgs e)
-        {
-            dynamic respuesta = dBApi.Get("https://pokeapi.co/api/v2/pokemon/" + txtpokemon.Text + "");
-            pictureBox2.ImageLocation = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/"+lblpoke.Text+".gif";
-        }
-    }
-
-    public class Persona
-    {
-        public string name { get; set; }
-        public string job { get; set; }
     }
 }
-
